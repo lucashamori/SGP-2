6158b9e666877538413c557f412e463b598cf862
Lucas Mori <57762259+lucashamori@users.noreply.github.com>
2025-10-27 23:38:27 -0300

    Remove Firebase configuration from firebase.ts
    Removed Firebase configuration object from the file.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,15 +8,7 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDrCQIu5cH7dxZ-argjqV2jbyG9v_dzFEE",
-  authDomain: "sgp-senac.firebaseapp.com",
-  projectId: "sgp-senac",
-  storageBucket: "sgp-senac.firebasestorage.app",
-  messagingSenderId: "136980853106",
-  appId: "1:136980853106:web:2e4832da4f8f65fd612deb",
-  measurementId: "G-FF0TY4DDT6"
-};
+
 
 // Initialize Firebase
 // Inicializa o Firebase (garante que só seja inicializado uma vez)
